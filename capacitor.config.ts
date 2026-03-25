34e5827b4bbccbb44a8eee277f38f92fd33b20a9
huangbinbin <huangbinbin@yunhexx.com>
2026-03-25 12:13:07 +0800

    Deploy to GitHub Pages

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.todo.app',
-  appName: 'TodoApp',
-  webDir: 'dist'
-};
-
-export default config;
